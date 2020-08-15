16a4a623bc59a684ce5c14f41a113ea018e0e90e
wuguanyang <32060343+wuguanyang@users.noreply.github.com>
2020-08-15 23:14:07 +0800

    wgy
    wow

---
+++ Assets/MonoSingleton.cs
@@ -0,0 +1,33 @@
+﻿/*
+CreateBy:     wgy
+CreateTime:   2020/08/13 15:53:06
+Description:  继承MonoBehaviour的单例模板
+*/
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EM {
+
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+        protected static T instance;
+
+        protected static GameObject instanceGameObject;
+
+        public static T Instance {
+            get {
+                if (instanceGameObject==null) {
+                    instanceGameObject = new GameObject (typeof (T).ToString ());
+                    instance = instanceGameObject.AddComponent<T> ();
+                    DontDestroyOnLoad (instanceGameObject);
+                }
+                return instance;
+            }
+        }
+        protected virtual void OnDestroy () {
+            instance = null;
+            instanceGameObject = null;
+        }
+    }
+
+}
---
+++ Assets/Singleton.cs
@@ -0,0 +1,48 @@
+﻿/*
+CreateBy:     wgy
+CreateTime:   2020/08/13 15:53:06
+Description:  单例模板
+*/
+
+using System;
+using System.Reflection;
+
+namespace EM {
+    //
+    public abstract class Singleton<T> where T : class {
+
+        protected static T instance;
+        public static T Instance {
+            get {
+                if (instance == null) {
+                    // 先获取所有非public的构造方法
+                    ConstructorInfo[] ctors = typeof (T).GetConstructors (BindingFlags.Instance | BindingFlags.NonPublic);
+                    // 从ctors中获取无参的构造方法
+                    ConstructorInfo ctor = Array.Find (ctors, c => c.GetParameters ().Length == 0);
+                    if (ctor == null)
+                        throw new Exception ("Non-public ctor() not found!");
+                    // 调用构造方法
+                    instance = ctor.Invoke (null) as T;
+                }
+                return instance;
+            }
+        }
+        protected Singleton () {
+
+        }
+
+    }
+
+}
+public class XXXManager {
+    private static XXXManager instance = null;
+    public static XXXManager Instance {
+        get {
+            if (instance == null) {
+                instance = new XXXManager ();
+            }
+            return instance;
+        }
+    }
+    private XXXManager () { }
+}
